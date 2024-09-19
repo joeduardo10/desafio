b946481c4d36292da48811c41196050ee02da2b3
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2024-09-19 20:22:14 -0300

    Add files via upload

---
+++ desafios alura/js-curso-2-desafio_1/app.js
@@ -0,0 +1,31 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML= 'hora do desafio';
+
+function hello(){
+    console.log('Olá Mundo! me livrei da maldição');
+}
+
+function alerta(){ 
+    alert('Eu amo JavaScript');
+}
+
+function nome_da_cidade(){
+    let cidade = prompt('digite o nome da sua cidade:')
+    alert(`Estive em ${cidade} e lembrei de você`);
+}
+
+function soma(){
+    let n1 = parseInt(prompt('Digite un número'));
+    let n2 = parseInt(prompt('Digite outro número')) ;
+    let resultado = parseInt(n1+n2);
+    alert(`${resultado}`);
+
+  
+function recebeNome(nome){
+    
+     console.log(`${nome}`);
+    
+}  
+  
+    
+} 
